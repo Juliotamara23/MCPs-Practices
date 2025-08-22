fe5fd03a1e476e10cf36a6d774bf8e3c9b814f67
julio.tamara23 <julio.tamara23@outlook.cm>
2025-08-22 15:10:08 -0500

    Agregando el adaptador para convertir la respuesta en el formato esperado por el servidor MCP

--- src/index.ts
+++ src/index.ts
@@ -5,40 +5,62 @@ import OpenAI from "openai";
 import { z } from "zod"; // Import zod for schema validation
 
 class MCPClient {
-  private openai: OpenAI;
-  private client: Client;
+    private openai: OpenAI;
+    private client: Client;
 
-  constructor() {
-    this.openai = new OpenAI({
-      baseURL: "https://models.inference.ai.azure.com",
-      apiKey: process.env.GITHUB_TOKEN,
-    });
+    constructor() {
+        this.openai = new OpenAI({
+            baseURL: "https://models.inference.ai.azure.com",
+            apiKey: process.env.GITHUB_TOKEN,
+        });
 
-    this.client = new Client(
-      {
-        name: "example-client",
-        version: "1.0.0",
-      },
-      {
-        capabilities: {
-          prompts: {},
-          resource: {},
-          tools: {},
-        },
-      }
-    );
-  }
+        this.client = new Client(
+            {
+                name: "example-client",
+                version: "1.0.0",
+            },
+            {
+                capabilities: {
+                    prompts: {},
+                    resource: {},
+                    tools: {},
+                },
+            }
+        );
+    }
 
-  async connectToServer(transport: Transport) {
-    await this.client.connect(transport);
-    this.run();
-    console.error("MCPClient started on stdin/stdout");
-  }
+    async connectToServer(transport: Transport) {
+        await this.client.connect(transport);
+        this.run();
+        console.error("MCPClient started on stdin/stdout");
+    }
+
+    openAIToolAdapter(tool: {
+        name: string;
+        description: string;
+        input_schema: any;
+    }) {
+        // Create a zod schema based on the input_schema
+        const schema = z.object(tool.input_schema);
+
+        return {
+            type: "function" as const, // Explicitly set type to "function"
+            function: {
+                name: tool.name,
+                description: tool.description,
+                parameters: {
+                    type: "object",
+                    properties: tool.input_schema.properties,
+                    required: tool.input_schema.required,
+                },
+            }
+        }
 
   async run() {
-    console.log("Asking server for available tools");
+            console.log("Asking server for available tools");
 
-    //listing tools
-    const toolsResult = await this.client.listTools();
-  }
+            //listing tools
+            const toolsResult = await this.client.listTools();
+        }
+    }
 }
