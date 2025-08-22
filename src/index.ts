2d357b51b7d731f92cd33e727478a07a80a5feb3
julio.tamara23 <julio.tamara23@outlook.cm>
2025-08-22 12:57:26 -0500

    Creando el cliente

---
+++ src/index.ts
@@ -0,0 +1,31 @@
+import { Client } from "@modelcontextprotocol/sdk/client/index.js";
+import { StdioClientTransport } from "@modelcontextprotocol/sdk/client/stdio.js";
+import { Transport } from "@modelcontextprotocol/sdk/shared/transport.js";
+import OpenAI from "openai";
+import { z } from "zod"; // Import zod for schema validation
+
+class MCPClient {
+  private openai: OpenAI;
+  private client: Client;
+
+  constructor() {
+    this.openai = new OpenAI({
+      baseURL: "https://models.inference.ai.azure.com",
+      apiKey: process.env.GITHUB_TOKEN,
+    });
+
+    this.client = new Client(
+      {
+        name: "example-client",
+        version: "1.0.0",
+      },
+      {
+        capabilities: {
+          prompts: {},
+          resource: {},
+          tools: {},
+        },
+      }
+    );
+  }
+}
