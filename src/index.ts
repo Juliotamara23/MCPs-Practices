dac6e2fee4351e4639f074bc236632f3e25298cf
julio.tamara23 <julio.tamara23@outlook.cm>
2025-09-02 15:57:44 -0500

    Eliminando las rutas innecesarias, corrigiendo errores de escritura.

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,53 @@
-import express from "express";
-import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
 import { SSEServerTransport } from "@modelcontextprotocol/sdk/server/sse.js";
+import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
+import { Request, Response } from "express";
+const express = require("express");
 
-export const server = new McpServer({
+const server = new McpServer({
   name: "Example Server",
   version: "1.0.0",
 });
 
+const app = express();
+const transports: { [sessionId: string]: SSEServerTransport } = {};
+
+app.get("/sse", async (_: Request, res: Response) => {
+  const transport = new SSEServerTransport("/messages", res);
+  transports[transport.sessionId] = transport;
+  res.on("close", () => {
+    delete transports[transport.sessionId];
+  });
+  await server.connect(transport);
+});
+
+app.post("/messages", async (req: Request, res: Response) => {
+  const sessionId = req.query.sessionId as string;
+  const transport = transports[sessionId];
+  if (transport) {
+    await transport.handlePostMessage(req, res);
+  } else {
+    res.status(400).send("No se encontró transporte para la sessionId");
+  }
+});
+
 server.tool(
   "random-joke",
-  "Un chiste devuuelto por la API de Chuck Norris",
+  "Un chiste devuelto por la API de Chuck Norris",
   {},
   async () => {
     const response = await fetch("https://api.chucknorris.io/jokes/random");
     const data = await response.json();
-    
+
     return {
-        content: [{
-            type: "text",
-            text: data.value
-        }]
-    }
+      content: [
+        {
+          type: "text",
+          text: data.value,
+        },
+      ],
+    };
   }
 );
 
-const app = express();
-export const transports: { [sessinId: string]: SSEServerTransport } = {};
-
 app.listen(3001);
+console.log("✅ Server is running on port 3001 successfully!");
