9a1404798bf6043b8867ed96f54fa9c8e3c80b7d
julio.tamara23 <julio.tamara23@outlook.cm>
2025-09-04 12:50:31 -0500

    Mejorando la descripción de las herramientas del servidor y cambiando la forma en que levanta el servidor.

--- src/index.ts
+++ src/index.ts
@@ -11,6 +11,7 @@ const server = new McpServer({
 const app = express();
 const transports: { [sessionId: string]: SSEServerTransport } = {};
 
+// Endpoint para establecer la conexión SSE y recibir mensajes.
 app.get("/sse", async (_: Request, res: Response) => {
   const transport = new SSEServerTransport("/messages", res);
   transports[transport.sessionId] = transport;
@@ -29,6 +30,37 @@ app.post("/messages", async (req: Request, res: Response) => {
     res.status(400).send("No se encontró transporte para la sessionId");
   }
 });
+
+// Herramientas: Chistes aleatorios y por categoría
+server.tool(
+  "joke-category",
+  "Categoria de chiste devuelto por la API de Chuck Norris",
+  {},
+  async () => {
+    const response = await fetch("https://api.chucknorris.io/jokes/categories");
+    const data = await response.json();
+
+    if (Array.isArray(data)) {
+      return {
+        content: [
+          {
+            type: "text",
+            text: data.map((category) => `• ${category}`).join("\n"),
+          },
+        ],
+      };
+    } else {
+      return {
+        content: [
+          {
+            type: "text",
+            text: data,
+          },
+        ],
+      };
+    }
+  }
+);
 
 server.tool(
   "random-joke",
@@ -49,5 +81,10 @@ server.tool(
   }
 );
 
-app.listen(3001);
-console.log("✅ Server is running on port 3001 successfully!");
+const PORT = 3001;
+app.listen(PORT, () => {
+  console.log(`✅ El servidor se levanto correctamente en el puerto ${PORT}`);
+}),
+  (error: string) => {
+    console.error(`❌ Error al iniciar el servidor: ${error}`);
+  };
