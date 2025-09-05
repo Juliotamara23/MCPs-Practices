cc49b0a9ff4957e248a2c72a648043bbf0b41788
julio.tamara23 <julio.tamara23@outlook.cm>
2025-09-05 13:54:33 -0500

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+import { StreamableHTTPServerTransport } from "@modelcontextprotocol/sdk/server/streamableHttp.js";
+import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
+import { Request, Response } from "express";
+import z from "zod";
+const express = require("express");
+
